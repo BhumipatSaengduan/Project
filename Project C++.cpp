f067628ed44c19d5deff51a9bdc35b4a6b56a65a
Bhumipat <bhumipat2004@gmail.com>
2024-02-20 13:06:13 +0700

    first commit

---
+++ Project C++.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void customerBuy() { // 1.Apple 2.Banana 3.Bacon 4.Egg 5.Bread 6.Cearal 7.Milk 8.Coffee 9.Soap 10.Shampoo
+	int n = 1;
+	char apple, banana, bacon, egg, bread, cereal, milk, coffee, soap, shampoo;
+	cout << "What did customer buy?" << endl;
+	cout << "DAY " << n << ": " << endl;
+	cout << "Buy Apple (Y/N)";
+	cin >> apple;
+	cout << "Buy Banana (Y/N)";
+	cin >> banana;
+	cout << "Buy Bacon (Y/N)";
+	cin >> bacon;
+	cout << "Buy Egg (Y/N)";
+	cin >> egg;
+	cout << "Buy Bread (Y/N)";
+	cin >> bread;
+	cout << "Buy Cereal (Y/N)";
+	cin >> cereal;
+	cout << "Buy Milk (Y/N)";
+	cin >> milk;
+	cout << "Buy Coffee (Y/N)";
+	cin >> coffee;
+	cout << "Buy Soap (Y/N)";
+	cin >> soap;
+	cout << "Buy Shampoo (Y/N)";
+	cin >> shampoo;
+}
+	
+
+
+int main() {
+
+
+}
