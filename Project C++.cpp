06ac33cb2a73ac2c7f1f798d710c663e5912b053
BhumipatSaengduan <152416744+BhumipatSaengduan@users.noreply.github.com>
2024-03-01 14:05:47 +0700

    Update Project C++.cpp

--- Project C++.cpp
+++ Project C++.cpp
@@ -1,101 +1,141 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-const int N  = 10;
-// Function to find the index of the product with the highest sales
-int findMaxSalesIndex(const int sales[]) {
-    int maxsold = 0;
-    for (int i = 1; i < N; i++) {
-        if (sales[i] > sales[maxsold]) {
-            maxsold = i;
-        }
+const int N = 10;
+
+void findMaxSalesIndexes(int sales[N], bool is_max[N]) {
+    int max = 0;
+    for (int i = 0; i < N; i++) {
+        if (sales[i] > max) max = sales[i];
     }
-    return maxsold;
+
+    for (int i = 0; i < N; i++) {
+        if (sales[i] == max) is_max[i] = true;
+        else is_max[i] = false;
+    }
 }
 
-// Function to find the index of the product with the lowest sales
-int findMinSalesIndex(const int sales[]) {
-    int minsold = 0;
-    for (int i = 1; i < N; i++) {
-        if (sales[i] < sales[minsold]) {
-            minsold = i;
-        }
+void findMinSalesIndexes(int sales[N], bool is_min[N]) {
+    int min = 0;
+    for (int i = 0; i < N; i++) {
+        if (sales[i] < min) min = sales[i];
     }
-    return minsold;
+
+    for (int i = 0; i < N; i++) {
+        if (sales[i] == min) is_min[i] = true;
+        else is_min[i] = false;
+    }
 }
 
 int main() {
-    string products[N] = {"Apple", "Banana", "Bacon", "Egg", "Bread","Cereal", "Milk", "Coffee", "Soap", "Shampoo"};
-    int prices[N] = {10, 5, 50, 7, 15, 20, 10, 15, 18, 30};
+    string products[N] = { "Apple", "Banana", "Bacon", "Egg", "Bread","Cereal", "Milk", "Coffee", "Soap", "Shampoo" };
+    int prices[N] = { 10, 5, 50, 7, 15, 20, 10, 15, 18, 30 };
     int totalRe = 0;
-    int totalsales[N] = {0};
+    int totalsales[N] = { 0 };
 
-    while(true) {
-        int sales[N] = {0}; 
-        int totalCost = 0; 
-        string name ;
+    while (true) {
+        int sales[N] = { 0 };
+        int totalCost = 0;
+        int currentTotalSales[N] = { 0 };
+        string name;
 
         cout << "Hello, Welcome to the 8-12 store.\n";
         cout << "This is an automatic response message for online orders from our store.\n";
-        cout << "Can you please tell me the name?"<<endl;
-        getline(cin,name);
-        cout << "Hello, Mr. "<< name << endl;
+        cout << "Can you please tell me the name?" << endl;
+        cout << "Enter your name: ";
+        getline(cin, name);
+        cout << "Hello, Mr. " << name << endl;
         cout << "Here is a list of all orders you can purchase. Please enter the quantity of the item to be purchased, or enter 0 to not purchase the item." << endl;
 
         for (int i = 0; i < N; i++) {
-            int selection;
             cout << "How many " << products[i] << " do you want to buy? (0 for No): ";
-            cin >> selection;
+            string selection_user;
+            getline(cin, selection_user);
+
+            int selection;
+            if (selection_user != "No") {
+                char* endptr;
+                int value = strtol(selection_user.c_str(), &endptr, 10);
+                if (endptr == selection_user || *endptr != '\0') {
+                    selection = -1;
+                }
+                else selection = value;
+            }
+            else continue;
 
             if (selection > 0) {
                 totalCost += selection * prices[i];
                 sales[i] += selection;
-            } else if (selection < 0) {
+            }
+            else if (selection < 0) {
                 cout << "Wrong input. Please enter a non-negative integer or 0 for No." << endl;
                 i--; // Repeat the iteration for the same product
             }
         }
 
-        cout << "\nHere are all the items Mr. "<< name<< " ordered." << endl;
+        cout << "\nHere are all the items Mr. " << name << " ordered." << endl;
         for (int i = 0; i < N; i++) {
             int totalsold = sales[i] * prices[i];
             if (totalsold > 0)
                 cout << products[i] << ": " << totalsold << " Baht" << endl;
-                totalsales[i] += sales[i];
+            currentTotalSales[i] += sales[i];
         }
-
-        totalRe += totalCost;
-
+        // Discount
+        //string Allmember;
+            /*cout << "Do you have Allmember?" << endl;
+            if (Allmember == "Yes") {
+                
+            }*/
         cout << "\nTotal cost: " << totalCost << " Baht" << endl;
+        cout << "Please check whether your order is correct or not." << endl;
+        cout << "If correct, please enter Correct. If not, enter No." << endl;
         string check;
-        cout << "Please check whether your order is correct or not."<<endl;
-        cout << "If correct, please enter Correct. If not, enter No."<<endl;
-        cin >> check ;
-        if (check == "correct" || check =="Correct" ){
-             cout << "Thank you for using the service."<<endl;
-             cout <<" Have a good day :) " <<endl;
-             string Endday;
-             cout << "Enter 'Close' to end the day or input anything else to continue: ";
-             cin >> Endday;
-             if (Endday == "Close" || Endday == "close") break;
+        getline(cin, check);
+        if (check == "correct" || check == "Correct") {
+            totalRe += totalCost;
+            for (int i = 0; i < N; i++) {
+                totalsales[i] += currentTotalSales[i];
+            }
+
+            cout << "Thank you for using the service." << endl;
+            cout << "Have a good day :)\n\n\n\n\n\n" << endl;
+            cout << "Enter 'Close' to end the day or input anything else to continue: ";
+            string endday;
+            getline(cin, endday);
+            cout << "\n\n\n\n\n";
+            if (endday == "Close" || endday == "close") break;
         }
         else {
-            
+            cout << "We apologize for the inconvenience caused. You can reorder by here.\n\n";
+            continue;
         }
     }
 
-    // part seller
+    // Part seller
     cout << "\nToday's Summary: " << endl;
 
-    int maxSalesIndex = findMaxSalesIndex(totalsales);
-    cout << "Best selling product: " << products[maxSalesIndex] << " (" << totalsales[maxSalesIndex] << " units)" << endl;
+    bool maxSalesIndexes[N] = { false };
+    findMaxSalesIndexes(totalsales, maxSalesIndexes);
 
-    int minSalesIndex = findMinSalesIndex(totalsales);
-    cout << "Worst selling product: " << products[minSalesIndex] << " (" << totalsales[minSalesIndex] << " units)" << endl;
+    cout << "Best selling product:" << endl;
+    for (int i = 0; i < N; i++) {
+        if (maxSalesIndexes[i]) {
+            cout << "  - " << products[i] << " (" << totalsales[i] << " units) " << endl;
+        }
+    }
 
-    cout << "Total revenue: " << totalRe << " Bath" << endl;
+    bool minSalesIndexes[N] = { false };
+    findMinSalesIndexes(totalsales, minSalesIndexes);
+
+    cout << "Worst selling product:" << endl;
+    for (int i = 0; i < N; i++) {
+        if (minSalesIndexes[i]) {
+            cout << "  - " << products[i] << " (" << totalsales[i] << " units) " << endl;
+        }
+    }
+
+    cout << "Total revenue: " << totalRe << " Baht" << endl;
 
     return 0;
 }
